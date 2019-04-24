07935bb1b653696ab8bf72f782f08ebb69bdab10
Antonio Verni <me.verni@gmail.com>
2019-04-24 19:48:01 +0200

    Configured swagger to scan only org.isf packages

--- src/main/java/org/isf/config/SpringFoxConfig.java
+++ src/main/java/org/isf/config/SpringFoxConfig.java
@@ -2,7 +2,6 @@ package org.isf.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
@@ -16,7 +15,8 @@ public class SpringFoxConfig {
     public Docket apiDocket() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("org.isf"))
+                //.apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build();
     }
