88012b8751259182ee9c8b6ab30cc7e40b3d0566
Antonio Verni <me.verni@gmail.com>
2019-04-24 19:46:00 +0200

    First add

---
+++ src/main/java/org/isf/security/OHFilterInvocationSecurityMetadataSource.java
@@ -0,0 +1,28 @@
+package org.isf.security;
+
+import java.util.Collection;
+
+import org.springframework.security.access.ConfigAttribute;
+import org.springframework.security.web.access.intercept.FilterInvocationSecurityMetadataSource;
+
+public class OHFilterInvocationSecurityMetadataSource implements FilterInvocationSecurityMetadataSource {
+
+	@Override
+	public Collection<ConfigAttribute> getAttributes(Object object) throws IllegalArgumentException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Collection<ConfigAttribute> getAllConfigAttributes() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean supports(Class<?> clazz) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
