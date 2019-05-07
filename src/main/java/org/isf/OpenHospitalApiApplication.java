7127c8cff890df0d202a5636dcb27dfaa07fff4d
Antonio Verni <me.verni@gmail.com>
2019-05-07 17:23:58 +0200

    Removed local applicationContext.xml

--- src/main/java/org/isf/OpenHospitalApiApplication.java
+++ src/main/java/org/isf/OpenHospitalApiApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
 
-@ImportResource({"classpath:*applicationContext.xml"})
+@ImportResource({"classpath*:**/applicationContext.xml"})
 @SpringBootApplication
 public class OpenHospitalApiApplication {
 
