883d421e1622f20b06459d5a6ba4ea64ef97021d
Vito Romano <vito988@gmail.com>
2019-11-27 19:33:45 +0100

    OP-127 resource bundle to manage exceptiuon error messages #time 2h

--- src/main/java/org/isf/shared/controller/ResponseCode.java
+++ src/main/java/org/isf/shared/controller/ResponseCode.java
@@ -2,7 +2,8 @@ package org.isf.shared.controller;
 
 public enum ResponseCode {
 
-	SUCCESS("success"), INVALID_PARAMETER("Invalid parameter error"), INTERNAL_SERVER_ERROR("Internal Server Error"), NOT_FOUND("Resource Not Found");
+	SUCCESS("success"), INVALID_PARAMETER("Invalid parameter error"), VALIDATION_ERROR("Validation errors"),
+	INTERNAL_SERVER_ERROR("Internal Server Error"), NOT_FOUND("Resource Not Found");
 
 	private String value;
 
--- src/main/java/org/isf/shared/exceptions/OHResponseEntityExceptionHandler.java
+++ src/main/java/org/isf/shared/exceptions/OHResponseEntityExceptionHandler.java
@@ -1,17 +1,22 @@
 package org.isf.shared.exceptions;
 
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
 
 import org.isf.shared.controller.FailureDto;
 import org.isf.shared.controller.ResponseCode;
 import org.isf.shared.controller.ValidationErrorDto;
 import org.isf.utils.exception.OHServiceException;
+import org.isf.utils.exception.OHServiceValidationException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -25,10 +30,16 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 @RestControllerAdvice
 public class OHResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
 
-	@ExceptionHandler(value = { OHServiceException.class })
+	@Autowired
+	private MessageSource messageSource;
+
+	@ExceptionHandler(value = { OHServiceValidationException.class })
 	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-	protected FailureDto handleOHServiceException(OHServiceException ex) {
-		return new FailureDto(ResponseCode.INTERNAL_SERVER_ERROR, ResponseCode.INTERNAL_SERVER_ERROR.getValue());
+	protected ValidationErrorDto handleOHServiceValidationException(OHServiceValidationException ex, Locale locale) {
+		final Map<String, Object> errors = new HashMap<>();
+		ex.getMessages()
+				.forEach(m -> errors.put(m.getMessage(), messageSource.getMessage(m.getMessage(), null, locale)));
+		return new ValidationErrorDto(ResponseCode.VALIDATION_ERROR.getValue(), errors);
 	}
 
 	@ExceptionHandler(value = { OHAPINotFoundException.class })
@@ -37,19 +48,43 @@ public class OHResponseEntityExceptionHandler extends ResponseEntityExceptionHan
 		return new FailureDto(ResponseCode.NOT_FOUND, ex.getMessage());
 	}
 
+	@ExceptionHandler(value = { OHServiceException.class })
+	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+	protected FailureDto handleOHServiceException(Exception ex) {
+		return new FailureDto(ResponseCode.INTERNAL_SERVER_ERROR, ResponseCode.INTERNAL_SERVER_ERROR.getValue());
+	}
+
+	@ExceptionHandler(value = { Exception.class })
+	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+	protected FailureDto handleGenericException(Exception ex) {
+		return new FailureDto(ResponseCode.INTERNAL_SERVER_ERROR, ResponseCode.INTERNAL_SERVER_ERROR.getValue());
+	}
+
 	@Override
 	@ResponseStatus(HttpStatus.BAD_REQUEST)
-	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
+
 		final Map<String, Object> errors = new HashMap<>();
+
 		for (final FieldError error : ex.getBindingResult().getFieldErrors()) {
 			errors.put(error.getField(), error.getDefaultMessage());
 		}
 		for (final ObjectError error : ex.getBindingResult().getGlobalErrors()) {
 			errors.put(error.getObjectName(), error.getDefaultMessage());
 		}
-		final ValidationErrorDto validaErrorDto = new ValidationErrorDto(ResponseCode.INVALID_PARAMETER.getValue(), errors);
-		return handleExceptionInternal(ex, validaErrorDto, headers, HttpStatus.BAD_REQUEST,
-				request);
+		final ValidationErrorDto validaErrorDto = new ValidationErrorDto(ResponseCode.INVALID_PARAMETER.getValue(),
+				errors);
+		return handleExceptionInternal(ex, validaErrorDto, headers, HttpStatus.BAD_REQUEST, request);
+	}
+
+	@Override
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
+		return handleExceptionInternal(ex,
+				new FailureDto(ResponseCode.INVALID_PARAMETER, ResponseCode.INVALID_PARAMETER.getValue()), headers,
+				HttpStatus.BAD_REQUEST, request);
 	}
 
 }
