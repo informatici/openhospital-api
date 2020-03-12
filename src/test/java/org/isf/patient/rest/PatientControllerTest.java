9a5dea4ecb3df13fb75a93c4a55613223b3eaaf8
Emerson Castaneda <emecas@users.noreply.github.com>
2020-03-11 20:43:35 -0400

    Add progresses on OP-121 (PUT) - WIP

--- src/test/java/org/isf/patient/rest/PatientControllerTest.java
+++ src/test/java/org/isf/patient/rest/PatientControllerTest.java
@@ -4,11 +4,16 @@
 package org.isf.patient.rest;
 
 import static org.hamcrest.Matchers.containsString;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertSame;
 import static org.junit.Assert.fail;
 import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -27,6 +32,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -110,7 +116,7 @@ public class PatientControllerTest {
 		String request = "/patients";
 		PatientDTO newPatient =  new PatientDTO();
 		
-		when(patientBrowserManagerMock.getPatient(any(String.class))).thenReturn(null);
+		//when(patientBrowserManagerMock.getPatient(any(String.class))).thenReturn(null);
 		
 		this.mockMvc
 			.perform(post(request).content(newPatient.getBlobPhoto()))
@@ -171,12 +177,85 @@ public class PatientControllerTest {
 	
 	/**
 	 * Test method for {@link org.isf.patient.rest.PatientController#updatePatient(int, org.isf.patient.dto.PatientDTO)}.
+	 * @throws Exception 
 	 */
 	@Test
-	public void testUpdatePatient() {
-		fail("Not yet implemented");
+	public void when_put_update_patient_with_valid_body_and_existent_code_then_OK() throws Exception {
+		
+		Integer code= 12345;
+		String request = "/patients/{code}";
+		PatientDTO newPatientDTO =  PatientDTOHelper.setup();
+		newPatientDTO.setCode(code);
+		Patient	newPatient = PatientDTOHelper.setupPatient();
+		newPatient.setCode(code);
+	
+		when(patientBrowserManagerMock.updatePatient(any(Patient.class))).thenReturn(true);
+				
+		this.mockMvc
+		.perform(
+				put(request, code)
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(PatientDTOHelper.asJsonString(newPatientDTO).getBytes()))
+		.andDo(print())
+		.andExpect(status().isOk())
+		.andExpect(content().string(containsString(code.toString())));
 	}
 
+	/**
+	 * Test method for {@link org.isf.patient.rest.PatientController#updatePatient(int, org.isf.patient.dto.PatientDTO)}.
+	 * @throws Exception 
+	 */
+	@Test
+	public void when_put_update_patient_with_invalid_body_and_existent_code_then_BadRequest() throws Exception {
+		
+		Integer code= 12345;
+		String request = "/patients/{code}";
+					
+		MvcResult result = this.mockMvc
+		.perform(
+				put(request, code)
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(new byte[3]))
+		.andDo(print())
+		.andExpect(status().isBadRequest())
+		.andReturn();
+		
+		String content = result.getResponse().getContentAsString();
+		assertEquals("", content);
+	}
+	
+	
+	/**
+	 * Test method for {@link org.isf.patient.rest.PatientController#updatePatient(int, org.isf.patient.dto.PatientDTO)}.
+	 * @throws Exception 
+	 */
+	@Test
+	public void when_put_update_patient_with_valid_body_and_unexistent_code_then_Exception() throws Exception {
+		
+		Integer code= 12345;
+		String request = "/patients/{code}";
+		PatientDTO newPatientDTO =  PatientDTOHelper.setup();
+		newPatientDTO.setCode(code);
+		Patient	newPatient = PatientDTOHelper.setupPatient();
+		newPatient.setCode(code);
+		
+		when(patientBrowserManagerMock.updatePatient(any(Patient.class))).thenReturn(false);
+					
+		this.mockMvc
+		.perform(
+				put(request, code)
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(PatientDTOHelper.asJsonString(newPatientDTO).getBytes()))
+		.andDo(print())
+		.andExpect(status().is4xxClientError())
+		.andExpect(status().isBadRequest())
+		.andExpect(content().string(containsString("")));
+		//.andExpect(status().isNoContent());  //TODO is not better Not Found than an exception as well for the GET than Non_Content?
+
+		
+	}
+	
+	
 	/**
 	 * Test method for {@link org.isf.patient.rest.PatientController#getPatients(java.lang.Integer, java.lang.Integer)}.
 	 */
