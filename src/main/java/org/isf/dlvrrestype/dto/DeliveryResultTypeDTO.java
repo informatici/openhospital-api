5153de4d56c68513830fdd5c7d062846b5748b5e
gildas <gildasdongmo@yahoo.fr>
2020-03-18 18:45:29 +0100

    OP-115 #Api delivery result type ok

--- src/main/java/org/isf/dlvrrestype/dto/DeliveryResultTypeDTO.java
+++ src/main/java/org/isf/dlvrrestype/dto/DeliveryResultTypeDTO.java
@@ -1,23 +1,17 @@
-package org.isf.admtype.dto;
+package org.isf.dlvrrestype.dto;
 
 import javax.validation.constraints.NotNull;
 
 import io.swagger.annotations.ApiModelProperty;
 
-/**
- * Not used anymore
- * @author antonio
- *
- */
+public class DeliveryResultTypeDTO {
 
-public class AdmissionTypeDTO {
-	
 	@NotNull
-	@ApiModelProperty(notes = "code of the admission type", example="A", position = 1)
+	@ApiModelProperty(notes = "code of the delivery result type", example="M", position = 1)
 	private String code;
 	
 	@NotNull
-	@ApiModelProperty(notes = "description of the admission type", example="AMBULANCE", position = 2)
+	@ApiModelProperty(notes = "description of the delivery result type", example="MORTALITÉ MATERNELLE", position = 2)
     private String description;
 	
 	public String getCode() {
