86555cda1eb413ccd309451630805ec5a5907da7
riccardo_costa <riccardo.costa@intesys.it>
2020-03-22 21:57:55 +0100

    Refactoring newOpd and getOpds, adding searchOpds (to check dateFrom and dateTo)

--- src/main/java/org/isf/opd/rest/OpdController.java
+++ src/main/java/org/isf/opd/rest/OpdController.java
@@ -16,12 +16,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -41,20 +40,44 @@ public class OpdController extends OHApiAbstractController<Opd, OpdDTO> {
     }
 
     @PostMapping(value = "/opds", produces = MediaType.APPLICATION_JSON_VALUE)
-    ResponseEntity<Integer> newOpd(@RequestBody OpdDTO newOpd) throws OHServiceException {
-        logger.info("Create opd for patient " + newOpd.getPatient().getCode());
-        boolean isCreated = opdManager.newOpd(toModel(newOpd));
-        Opd lastOpd = opdManager.getLastOpd(newOpd.getPatient().getCode());
-        if (!isCreated || lastOpd == null) {
-            throw new OHAPIException(new OHExceptionMessage(null, "Opd is not created!", OHSeverityLevel.ERROR));
-        }
-        return ResponseEntity.status(HttpStatus.CREATED).body(lastOpd.getCode());
+    ResponseEntity<Boolean> newOpd(@RequestBody OpdDTO newOpd) throws OHServiceException {
+        logger.info(String.format("Create opd for patient [%d]", newOpd.getPatient().getCode()));
+        Boolean createOpd = opdManager.newOpd(toModel(newOpd));
+        return ResponseEntity.status(HttpStatus.CREATED).body(createOpd);
     }
 
-    @GetMapping(value = "/opds", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<List<OpdDTO>> getOpds(int patientcode) throws OHServiceException {
-        logger.info("Get opds");
+    @GetMapping(value = "/opds/{patientcode}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<OpdDTO>> getOpds(@PathVariable Integer patientcode) throws OHServiceException {
+        logger.info(String.format("Get opds by patientcode [%d]", patientcode));
         ArrayList<Opd> opds = opdManager.getOpdList(patientcode);
+        List<OpdDTO> opdDTOS = toDTOList(opds);
+        if (opdDTOS.size() == 0) {
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).body(opdDTOS);
+        } else {
+            return ResponseEntity.ok(opdDTOS);
+        }
+    }
+
+    @GetMapping(value = "/opds/search", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<OpdDTO>> searchOpds(
+            @RequestParam(value = "diseaseTypeCode", required = false, defaultValue = "") String diseaseTypeCode,
+            @RequestParam(value = "diseaseCode", required = false, defaultValue = "") String diseaseCode,
+            @RequestParam(value = "dateFrom", required = false) GregorianCalendar dateFrom,
+            @RequestParam(value = "dateTo", required = false) GregorianCalendar dateTo,
+            @RequestParam(value = "ageFrom", required = false, defaultValue = "0") Integer ageFrom,
+            @RequestParam(value = "ageTo", required = false, defaultValue = "0") Integer ageTo,
+            @RequestParam(value = "sex", required = false, defaultValue = "M") char sex,
+            @RequestParam(value = "newPatient", required = false, defaultValue = "R") char newPatient)  throws OHServiceException {
+        logger.info(String.format("Search opds by diseaseTypeCode [%s], diseaseCode [%s], dateFrom [%s], dateTo [%s], ageFrom [%d], ageTo [%d], sex [%c], newPatient [%c]"
+                , diseaseTypeCode, diseaseCode, dateFrom, dateTo, ageFrom, ageTo, sex, newPatient));
+        if (dateFrom == null) {
+            dateFrom = new GregorianCalendar();
+            dateFrom.add(Calendar.DAY_OF_MONTH, - 7);
+        }
+        if (dateTo == null) {
+            dateTo = new GregorianCalendar();
+        }
+        ArrayList<Opd> opds = opdManager.getOpd(diseaseTypeCode, diseaseCode, dateFrom, dateTo, ageFrom, ageTo, sex, newPatient);
         List<OpdDTO> opdDTOS = toDTOList(opds);
         if (opdDTOS.size() == 0) {
             return ResponseEntity.status(HttpStatus.NO_CONTENT).body(opdDTOS);
