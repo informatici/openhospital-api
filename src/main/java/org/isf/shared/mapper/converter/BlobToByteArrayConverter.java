e625ffb94dd60e011f33b16a201b0d289eb3b6dd
Alessandro Domanico <alessandro.domanico@yahoo.it>
2020-10-09 15:26:44 +0200

    Restored @Component annotation after deleting in 1f60d53

--- src/main/java/org/isf/shared/mapper/converter/BlobToByteArrayConverter.java
+++ src/main/java/org/isf/shared/mapper/converter/BlobToByteArrayConverter.java
@@ -4,6 +4,7 @@ import java.sql.Blob;
 import java.sql.SQLException;
 
 import org.modelmapper.AbstractConverter;
+import org.springframework.stereotype.Component;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -12,6 +13,7 @@ import lombok.extern.slf4j.Slf4j;
  *
  */
 @Slf4j
+@Component
 public class BlobToByteArrayConverter extends AbstractConverter<Blob, byte[]> {
 
 	@Override
--- src/main/java/org/isf/shared/mapper/converter/ByteArrayToBlobConverter.java
+++ src/main/java/org/isf/shared/mapper/converter/ByteArrayToBlobConverter.java
@@ -6,8 +6,6 @@ import java.sql.SQLException;
 import javax.sql.rowset.serial.SerialBlob;
 
 import org.modelmapper.AbstractConverter;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import lombok.extern.slf4j.Slf4j;
@@ -17,6 +15,7 @@ import lombok.extern.slf4j.Slf4j;
  *
  */
 @Slf4j
+@Component
 public class ByteArrayToBlobConverter extends AbstractConverter<byte[], Blob> {
 	
 	@Override
