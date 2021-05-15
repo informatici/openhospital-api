4168324cb81311db93a0f7ad6a19e71f4e9892bb
riccardo_costa <riccardo.costa@intesys.it>
2021-05-15 16:36:36 +0200

    Disable "date as timestamp" jackson serialization feature

--- src/main/java/org/isf/OpenHospitalApiApplication.java
+++ src/main/java/org/isf/OpenHospitalApiApplication.java
@@ -21,16 +21,29 @@
  */
 package org.isf;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
+
+import javax.annotation.PostConstruct;
 
 @ImportResource({"classpath*:/applicationContext.xml"})
 @SpringBootApplication
 public class OpenHospitalApiApplication {
 
+	@Autowired
+	private ObjectMapper objectMapper;
+
     public static void main(String[] args) {
         SpringApplication.run(OpenHospitalApiApplication.class, args);
     }
+
+	@PostConstruct
+	public void setUp() {
+		objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+	}
 
 }
