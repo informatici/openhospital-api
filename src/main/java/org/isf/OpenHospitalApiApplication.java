9c7e4e300f60371347b3afeebd72173220bfd6c1
riccardo_costa <riccardo.costa@intesys.it>
2022-02-13 20:51:31 +0100

    Disabilitato il formato data come timestamp

--- src/main/java/org/isf/OpenHospitalApiApplication.java
+++ src/main/java/org/isf/OpenHospitalApiApplication.java
@@ -21,16 +21,30 @@
  */
 package org.isf;
 
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 
 @ImportResource({"classpath*:/applicationContext.xml"})
 @SpringBootApplication
 public class OpenHospitalApiApplication {
 
+	@Autowired
+	private ObjectMapper objectMapper;
+
     public static void main(String[] args) {
         SpringApplication.run(OpenHospitalApiApplication.class, args);
     }
+
+	@PostConstruct
+	public void setUp() {
+		objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+	}
 
 }
