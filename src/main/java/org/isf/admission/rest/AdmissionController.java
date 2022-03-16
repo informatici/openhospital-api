774b9f6158ab8d914b25370ffdc583ae7b353a46
ArnaudFonzam <gui.fofou@uni2growcameroun.com>
2022-03-16 16:19:22 +0100

    return status 404 in controller GET /admissions/current when patient  doesn't exist

--- src/main/java/org/isf/admission/rest/AdmissionController.java
+++ src/main/java/org/isf/admission/rest/AdmissionController.java
@@ -155,10 +155,8 @@ public class AdmissionController {
 			throws OHServiceException {
 		LOGGER.info("Get admission by patient code: {}", patientCode);
 		Patient patient = patientManager.getPatientById(patientCode);
-		if (patient == null) {
-			throw new OHAPIException(new OHExceptionMessage(null, "Patient not found!", OHSeverityLevel.ERROR),
-					HttpStatus.INTERNAL_SERVER_ERROR);
-		}
+		if (patient == null)
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
 		Admission admission = admissionManager.getCurrentAdmission(patient);
 		if (admission == null) {
 			return ResponseEntity.status(HttpStatus.NO_CONTENT).body(null);
