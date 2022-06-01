17d365b7e1372dbf380cd77e6bca2096a6204aea
ArnaudFonzam <gui.fofou@uni2growcameroun.com>
2022-06-01 16:04:30 +0100

    version stable de main

--- src/main/java/org/isf/disease/rest/DiseaseController.java
+++ src/main/java/org/isf/disease/rest/DiseaseController.java
@@ -222,7 +222,7 @@ public class DiseaseController {
 	 * @throws OHServiceException
 	 */
 	@GetMapping(value = "/diseases/{code}", produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<DiseaseDTO> getDiseaseByCode(@PathVariable("code") Integer code) throws OHServiceException {
+	public ResponseEntity<DiseaseDTO> getDiseaseByCode(@PathVariable("code") String code) throws OHServiceException {
         LOGGER.info("Get disease by code");
 	    Disease disease = diseaseManager.getDiseaseByCode(code);
 	    if(disease != null) {
@@ -281,7 +281,7 @@ public class DiseaseController {
 	 * @throws OHServiceException
 	 */
 	@DeleteMapping(value = "/diseases/{code}", produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<Map<String, Boolean>> deleteDisease(@PathVariable("code") Integer code) throws OHServiceException {
+	public ResponseEntity<Map<String, Boolean>> deleteDisease(@PathVariable("code") String code) throws OHServiceException {
 		Disease disease = diseaseManager.getDiseaseByCode(code);
 		if(disease != null) {
 			Map<String, Boolean> result = new HashMap<>();
