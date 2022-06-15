1c59eb8a549c05f7c7726851b5e59c50e7112101
ArnaudFonzam <gui.fofou@uni2growcameroun.com>
2022-06-15 15:34:30 +0100

    fix bug about opd

--- src/main/java/org/isf/opd/dto/OpdDTO.java
+++ src/main/java/org/isf/opd/dto/OpdDTO.java
@@ -42,7 +42,7 @@ public class OpdDTO {
     private LocalDateTime date;
 	//@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
 
-    @NotNull
+    //@NotNull
     @ApiModelProperty(notes = "the visit date", position = 3)
     private LocalDate visitDate;
 	//@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
@@ -58,7 +58,7 @@ public class OpdDTO {
     @ApiModelProperty(notes = "the patient age", example = "18", position = 6)
     private int age;
 
-   // @NotNull
+   //@NotNull
     @ApiModelProperty(notes = "the patient sex", example = "M", position = 7)
     private char sex;
 
@@ -66,7 +66,7 @@ public class OpdDTO {
     @ApiModelProperty(notes = "the admission note", example = "this is out patient", position = 8)
     private String note; // ADDED: Alex
 
-    @NotNull
+   // @NotNull
     @ApiModelProperty(notes = "a progr. in year for each ward", example = "18", position = 9)
     private int prog_year;
 
