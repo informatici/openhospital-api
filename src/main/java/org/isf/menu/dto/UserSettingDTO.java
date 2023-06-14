cd146a8f14bb0a84631b8f8004ae9331f6d95119
ArnaudFonzam <101590821+ArnaudFonzam@users.noreply.github.com>
2023-06-14 15:15:19 +0100

    OH2-202: Create backend for users settings (#285)
    * create all class about userSetting
    
    * update: src/main/java/org/isf/menu/rest/UserSettingController.java
    
    * src/main/java/org/isf/menu/rest/UserSettingController.java
    
    * update and format the code
    
    * update doc api
    
    ---------
    
    Co-authored-by: ArnaudFonzam <gui.fofou@umi2growcameroun.com>
    Co-authored-by: David B Malkovsky <david@malkovsky.org>
    Co-authored-by: Alessandro Domanico <alessandro.domanico@yahoo.it>

---
+++ src/main/java/org/isf/menu/dto/UserSettingDTO.java
@@ -0,0 +1,66 @@
+/*
+ * Open Hospital (www.open-hospital.org)
+ * Copyright © 2006-2023 Informatici Senza Frontiere (info@informaticisenzafrontiere.org)
+ *
+ * Open Hospital is a free and open source software for healthcare data management.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * https://www.gnu.org/licenses/gpl-3.0-standalone.html
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+package org.isf.menu.dto;
+
+import javax.validation.constraints.NotNull;
+
+import io.swagger.annotations.ApiModelProperty;
+
+public class UserSettingDTO {
+	
+	@NotNull
+	@ApiModelProperty(notes="the id of the setting (must be unique)", example = "1", position = 1)	
+	private int id;
+	
+	@NotNull
+	@ApiModelProperty(notes="the the name of the setting", example = "dashboard", position = 2)
+	private String configName;
+	
+	@NotNull
+	@ApiModelProperty(notes="the the value of the user", position = 3)
+	private String configValue;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getConfigName() {
+		return configName;
+	}
+
+	public void setConfigName(String configName) {
+		this.configName = configName;
+	}
+
+	public String getConfigValue() {
+		return configValue;
+	}
+
+	public void setConfigValue(String configValue) {
+		this.configValue = configValue;
+	}
+
+}
