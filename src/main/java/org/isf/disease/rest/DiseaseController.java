74eccc780c2293ddefb85e10482a4c506dce9c19
David B Malkovsky <david@malkovsky.org>
2023-10-12 04:19:30 -0400

    Update DiseaseController.java

--- src/main/java/org/isf/disease/rest/DiseaseController.java
+++ src/main/java/org/isf/disease/rest/DiseaseController.java
@@ -307,11 +307,11 @@ public class DiseaseController {
 	
 	private ResponseEntity<List<DiseaseDTO>> computeResponse(List<Disease> diseases) {
 		List<DiseaseDTO> diseasesDTO = mapper.map2DTOList(diseases);
-        if (diseasesDTO.isEmpty()){
-            return ResponseEntity.status(HttpStatus.NO_CONTENT).body(diseasesDTO);
-        } else {
-            return ResponseEntity.ok(diseasesDTO);
-        }
+        	if (diseasesDTO.isEmpty()) {
+            		return ResponseEntity.status(HttpStatus.NO_CONTENT).body(diseasesDTO);
+        	} else {
+            		return ResponseEntity.ok(diseasesDTO);
+        	}
 	}
 
 }
