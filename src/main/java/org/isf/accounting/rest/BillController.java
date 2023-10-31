6b6576eba2e0b2ad9626065240096de505756874
mwithi <alessandro.domanico@yahoo.it>
2023-10-31 23:18:18 +0100

    Test again

--- src/main/java/org/isf/accounting/rest/BillController.java
+++ src/main/java/org/isf/accounting/rest/BillController.java
@@ -63,7 +63,7 @@ import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController(value = "/bills")
-@Tag(name = "Bills Test")
+@Tag(name = "Bills")
 @SecurityRequirement(name = "bearerAuth")
 public class BillController {
 
