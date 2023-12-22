668a5c4bc1031b7b847929eb3e3dec7198121d3a
mwithi <alessandro.domanico@yahoo.it>
2023-12-22 01:26:16 +0100

    Revert "update src/main/java/org/isf/login/rest/LoginController.java"
    This reverts commit e6a6ba7cbfc961389e0ce8b0be73cdae92fdc7fd.

--- src/main/java/org/isf/login/rest/LoginController.java
+++ src/main/java/org/isf/login/rest/LoginController.java
@@ -23,6 +23,7 @@ package org.isf.login.rest;
 
 import java.time.LocalDateTime;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
 import org.isf.login.dto.LoginRequest;
@@ -71,7 +72,7 @@ public class LoginController {
 	private static final Logger LOGGER = LoggerFactory.getLogger(LoginController.class);
 
 	@PostMapping(value = "/auth/login", produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<LoginResponse> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) throws OHAPIException {
+	public ResponseEntity<LoginResponse> authenticateUser(@Valid @RequestBody LoginRequest loginRequest, HttpServletRequest request) throws OHAPIException {
 		Authentication authentication = authenticationManager.authenticate(
 						new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword()));
 		SecurityContextHolder.getContext().setAuthentication(authentication);
