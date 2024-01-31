8d0194098b650b347b61d4e6d37628b7c3f5c95e
mwithi <alessandro.domanico@yahoo.it>
2024-01-31 21:55:03 +0100

    Restore contextLoads() test

--- src/test/java/org/isf/OpenHospitalApiApplicationTests.java
+++ src/test/java/org/isf/OpenHospitalApiApplicationTests.java
@@ -21,6 +21,7 @@
  */
 package org.isf;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -29,9 +30,8 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 @SpringBootTest
 public class OpenHospitalApiApplicationTests {
 
-//  FIXME: test application context loading	
-//	@Test
-//	public void contextLoads() {
-//	}
-
+	@Test
+	public void contextLoads() {
+		// this should work, but it slows down the tests
+	}
 }
