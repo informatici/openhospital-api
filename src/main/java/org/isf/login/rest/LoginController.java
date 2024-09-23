196abc5f2f0ad286e08a07d9faea8b7c8df4ee31
mwithi <alessandro.domanico@yahoo.it>
2024-09-23 22:50:04 +0200

    First version of refresh token

--- src/main/java/org/isf/login/rest/LoginController.java
+++ src/main/java/org/isf/login/rest/LoginController.java
@@ -28,10 +28,12 @@ import jakarta.validation.Valid;
 
 import org.isf.login.dto.LoginRequest;
 import org.isf.login.dto.LoginResponse;
+import org.isf.login.dto.TokenRefreshRequest;
 import org.isf.menu.manager.UserBrowsingManager;
 import org.isf.menu.model.User;
 import org.isf.security.CustomAuthenticationManager;
 import org.isf.security.jwt.TokenProvider;
+import org.isf.security.jwt.TokenValidationResult;
 import org.isf.sessionaudit.manager.SessionAuditManager;
 import org.isf.sessionaudit.model.SessionAudit;
 import org.isf.sessionaudit.model.UserSession;
@@ -40,6 +42,7 @@ import org.isf.utils.exception.OHServiceException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -49,6 +52,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.jsonwebtoken.JwtException;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
@@ -79,7 +83,8 @@ public class LoginController {
 		Authentication authentication = authenticationManager.authenticate(
 						new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword()));
 		SecurityContextHolder.getContext().setAuthentication(authentication);
-		String jwt = tokenProvider.generateJwtToken(authentication, true);
+		String jwt = tokenProvider.generateJwtToken(authentication, false);
+		String refreshToken = tokenProvider.generateRefreshToken(authentication);
 
 		String userDetails = (String) authentication.getPrincipal();
 		User user;
@@ -97,6 +102,26 @@ public class LoginController {
 			LOGGER.error("Unable to log user login in the session_audit table");
 		}
 
-		return ResponseEntity.ok(new LoginResponse(jwt, userDetails));
+		return ResponseEntity.ok(new LoginResponse(jwt, refreshToken, userDetails));
 	}
+
+	@PostMapping("/auth/refresh-token")
+	public ResponseEntity< ? > refreshToken(@RequestBody TokenRefreshRequest request) {
+		String refreshToken = request.getRefreshToken();
+
+		try {
+			if (tokenProvider.validateToken(refreshToken) == TokenValidationResult.VALID) {
+				String username = tokenProvider.getUsernameFromToken(refreshToken);
+				Authentication authentication = tokenProvider.getAuthenticationByUsername(username); // Retrieve user details
+				String newAccessToken = tokenProvider.generateJwtToken(authentication, false);
+
+				return ResponseEntity.ok(new LoginResponse(newAccessToken, refreshToken, username));
+			} else {
+				return ResponseEntity.badRequest().body("Invalid Refresh Token");
+			}
+		} catch (JwtException e) {
+			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Refresh token expired or invalid");
+		}
+	}
+
 }
