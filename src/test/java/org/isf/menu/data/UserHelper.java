9da8ad9251896f4d72b1bf3e385497e2ff264e69
mwithi <alessandro.domanico@yahoo.it>
2024-09-25 22:18:51 +0200

    Improve UserHelper

--- src/test/java/org/isf/menu/data/UserHelper.java
+++ src/test/java/org/isf/menu/data/UserHelper.java
@@ -25,13 +25,10 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-import org.isf.login.dto.LoginRequest;
-import org.isf.login.dto.LoginResponse;
-import org.isf.login.dto.TokenRefreshRequest;
-import org.isf.menu.dto.UserDTO;
 import org.isf.menu.model.User;
-import org.isf.menu.model.UserGroup;
 import org.isf.utils.exception.OHException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -47,6 +44,13 @@ import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
  */
 public class UserHelper {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(UserHelper.class);
+
+	private static final ObjectMapper objectMapper = new ObjectMapper()
+					.registerModule(new ParameterNamesModule())
+					.registerModule(new Jdk8Module())
+					.registerModule(new JavaTimeModule());
+
 	public static User generateUser() throws OHException {
 		User user = new User();
 		user.setUserGroupName(UserGroupHelper.generateUserGroup());
@@ -57,82 +61,23 @@ public class UserHelper {
 	}
 
 	public static List<User> generateUsers(int nbUsers) throws OHException {
-		UserGroup userGroup = UserGroupHelper.generateUserGroup();
-
 		return IntStream.range(0, nbUsers).mapToObj(i -> {
-			User user = new User();
-			user.setUserGroupName(userGroup);
-			user.setUserName("oh user");
-			user.setDesc("oh first user");
-			user.setPasswd("very strong password");
-			return user;
+			try {
+				return generateUser();
+			} catch (OHException e) {
+				LOGGER.error("Error generating user", e);
+				return null;
+			}
 		}).collect(Collectors.toList());
 	}
 
-	public static String asJsonString(UserDTO userDTO) {
+	// TODO: to be moved in a general package?
+	public static <T> String asJsonString(T object) {
 		try {
-			return new ObjectMapper()
-							.registerModule(new ParameterNamesModule())
-							.registerModule(new Jdk8Module())
-							.registerModule(new JavaTimeModule())
-							.writeValueAsString(userDTO);
+			return objectMapper.writeValueAsString(object);
 		} catch (JsonProcessingException e) {
-			e.printStackTrace();
+			LOGGER.error("Error converting object to JSON", e);
+			return null;
 		}
-
-		return null;
-	}
-
-	public static String asJsonString(List<UserDTO> users) {
-		try {
-			return new ObjectMapper()
-							.registerModule(new ParameterNamesModule())
-							.registerModule(new Jdk8Module())
-							.registerModule(new JavaTimeModule())
-							.writeValueAsString(users);
-		} catch (JsonProcessingException e) {
-			e.printStackTrace();
-		}
-
-		return null;
-	}
-
-	public static String asJsonString(TokenRefreshRequest request) {
-		try {
-			return new ObjectMapper()
-							.registerModule(new ParameterNamesModule())
-							.registerModule(new Jdk8Module())
-							.registerModule(new JavaTimeModule())
-							.writeValueAsString(request);
-		} catch (JsonProcessingException e) {
-			e.printStackTrace();
-		}
-		return null;
-	}
-
-	public static String asJsonString(LoginResponse loginResponse) {
-		try {
-			return new ObjectMapper()
-							.registerModule(new ParameterNamesModule())
-							.registerModule(new Jdk8Module())
-							.registerModule(new JavaTimeModule())
-							.writeValueAsString(loginResponse);
-		} catch (JsonProcessingException e) {
-			e.printStackTrace();
-		}
-		return null;
-	}
-
-	public static String asJsonString(LoginRequest loginRequest) {
-		try {
-			return new ObjectMapper()
-							.registerModule(new ParameterNamesModule())
-							.registerModule(new Jdk8Module())
-							.registerModule(new JavaTimeModule())
-							.writeValueAsString(loginRequest);
-		} catch (JsonProcessingException e) {
-			e.printStackTrace();
-		}
-		return null;
 	}
 }
