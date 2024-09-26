034c23db7e3cbddf26b59b96b74af530aecf4923
mwithi <alessandro.domanico@yahoo.it>
2024-09-26 19:19:55 +0200

    Remove unnecessary @Autowired

--- src/main/java/org/isf/login/rest/LoginController.java
+++ src/main/java/org/isf/login/rest/LoginController.java
@@ -41,7 +41,6 @@ import org.isf.shared.exceptions.OHAPIException;
 import org.isf.utils.exception.OHServiceException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -61,24 +60,18 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 @SecurityRequirement(name = "bearerAuth")
 public class LoginController {
 
-	@Autowired
 	private HttpSession httpSession;
 
-	@Autowired
 	private SessionAuditManager sessionAuditManager;
 
-	@Autowired
 	private TokenProvider tokenProvider;
 
-	@Autowired
 	private CustomAuthenticationManager authenticationManager;
 
-	@Autowired
 	private UserBrowsingManager userManager;
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(LoginController.class);
 
-	@Autowired
 	public LoginController(HttpSession httpSession,
 					SessionAuditManager sessionAuditManager,
 					TokenProvider tokenProvider,
