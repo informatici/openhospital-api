25e6cd686029422840e364140c0fcb5846e8d7bd
SteveGT96 <steve.tsala@intesys.it>
2024-10-23 08:25:46 +0100

    doc: Update swagger documentation

--- src/main/java/org/isf/exam/dto/ExamWithRowsDTO.java
+++ src/main/java/org/isf/exam/dto/ExamWithRowsDTO.java
@@ -28,8 +28,8 @@ import jakarta.validation.constraints.NotNull;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 public record ExamWithRowsDTO(
-	@NotNull @Schema(name = "exam", description = "The exam to mutate") ExamDTO exam,
-	@Schema(name = "rows", example = "['POSITIVE', 'NEGATIVE']", description = "Possible result for the exam(only for exams with procedure 1 and 2)") List<String> rows
+	@NotNull @Schema(name = "exam", description = "The exam to be changed") ExamDTO exam,
+	@Schema(name = "rows", example = "['>=12 (NORMAL)', 'IRREGULAR']", description = "Possible result for the exam(only for exams with procedure 1 and 2)") List<String> rows
 ) {
 
 }
