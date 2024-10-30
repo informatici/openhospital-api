daec382f3757bc458a8f6544402344a2ee651b12
SilverD3 <mrboornesilver@gmail.com>
2024-10-30 11:29:14 +0100

    chore: í¸í¸í¸í¸í¸merde!

--- src/main/java/org/isf/config/SecurityConfig.java
+++ src/main/java/org/isf/config/SecurityConfig.java
@@ -305,6 +305,10 @@ public class SecurityConfig {
 				.requestMatchers(HttpMethod.GET, "/reports/**").hasAnyAuthority("reports.read")
 				.requestMatchers(HttpMethod.PUT, "/reports/**").hasAuthority("reports.update")
 				.requestMatchers(HttpMethod.DELETE, "/reports/**").hasAuthority("reports.delete")
+				// Settings
+				.requestMatchers(HttpMethod.GET, "/settings/**").hasAnyAuthority("settings.read")
+				.requestMatchers(HttpMethod.PUT, "/settings/**").hasAuthority("settings.update")
+				.requestMatchers(HttpMethod.POST, "/settings/**").hasAuthority("settings.update")
 				// sms
 				.requestMatchers(HttpMethod.POST, "/sms/**").hasAuthority("sms.create")
 				.requestMatchers(HttpMethod.GET, "/sms/**").hasAnyAuthority("sms.read")
@@ -340,10 +344,6 @@ public class SecurityConfig {
 				.requestMatchers(HttpMethod.GET, "/wards/**").hasAnyAuthority("wards.read")
 				.requestMatchers(HttpMethod.PUT, "/wards/**").hasAuthority("wards.update")
 				.requestMatchers(HttpMethod.DELETE, "/wards/**").hasAuthority("wards.delete")
-				// Settings
-				.requestMatchers(HttpMethod.GET, "/settings/**").hasAnyAuthority("settings.read")
-				.requestMatchers(HttpMethod.PUT, "/settings/**").hasAuthority("settings.update")
-				.requestMatchers(HttpMethod.POST, "/settings/**").hasAuthority("settings.update")
 
 				.anyRequest().authenticated()
 			)
