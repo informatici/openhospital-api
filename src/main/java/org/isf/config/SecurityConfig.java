9b0b477073b802b9f05e6e612ca8cba142f45f25
mwithi <alessandro.domanico@yahoo.it>
2024-12-08 18:00:31 +0100

    Resolving AuthenticationProvider WARN
    We don't need at the moment to define an authenticationProvider because
    we have only one UserDetailsService implementation
    (org.isf.security.UserDetailsServiceImpl) and only one PasswordEncoder
    bean (BCryptPasswordEncoder), so Spring Security uses the default
    DaoAuthenticationProvider
    
    In the future, if we add more UserDetailsService implementations for
    multiple authentication data sources (e.g. Database, LDAP, Third-Party)
    we must define each provider and instruct the AuthenticationManager in
    which one to use for each case.

--- src/main/java/org/isf/config/SecurityConfig.java
+++ src/main/java/org/isf/config/SecurityConfig.java
@@ -36,12 +36,10 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.authorization.AuthorizationDecision;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -55,9 +53,6 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-
-	@Autowired
-	private UserDetailsService userDetailsService;
 
 	private final TokenProvider tokenProvider;
 
@@ -74,14 +69,6 @@ public class SecurityConfig {
 
 	@Autowired
 	private CustomLogoutHandler customLogoutHandler;
-
-	@Bean
-	public DaoAuthenticationProvider authenticationProvider() {
-		DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-		authProvider.setUserDetailsService(userDetailsService);
-		authProvider.setPasswordEncoder(encoder());
-		return authProvider;
-	}
 
 	@Bean
 	public PasswordEncoder encoder() {
