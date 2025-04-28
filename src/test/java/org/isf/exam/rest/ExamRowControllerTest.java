99b4f76a3ec69ad068015a46be4b98331956ab8b
dbm <david@malkovsky.org>
2025-04-28 15:16:47 -0400

    OP-1390 update Spring Boot version and open api

--- src/test/java/org/isf/exam/rest/ExamRowControllerTest.java
+++ src/test/java/org/isf/exam/rest/ExamRowControllerTest.java
@@ -52,9 +52,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -75,13 +75,13 @@ class ExamRowControllerTest {
 	@Autowired
 	private ExamMapper examMapper;
 
-	@MockBean
+	@MockitoBean
 	private ExamBrowsingManager examManager;
 
 	@Autowired
 	private ObjectMapper objectMapper;
 
-	@MockBean
+	@MockitoBean
 	private ExamRowBrowsingManager manager;
 
 	@Test
--- src/test/java/org/isf/examination/rest/ExaminationControllerTest.java
+++ src/test/java/org/isf/examination/rest/ExaminationControllerTest.java
@@ -48,9 +48,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -68,10 +68,10 @@ class ExaminationControllerTest {
 	@Autowired
 	private PatientExaminationMapper mapper;
 
-	@MockBean
+	@MockitoBean
 	private ExaminationBrowserManager manager;
 
-	@MockBean
+	@MockitoBean
 	private PatientBrowserManager patientBrowserManager;
 
 	@Autowired
