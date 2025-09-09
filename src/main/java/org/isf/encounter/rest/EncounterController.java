6c577f47593676d8181d897beec56cf7a9db0dea
FOFOU FONZAM Gui Arnaud <101590821+ArnaudFonzam@users.noreply.github.com>
2025-09-09 14:10:54 +0100

    Update src/main/java/org/isf/encounter/rest/EncounterController.java

--- src/main/java/org/isf/encounter/rest/EncounterController.java
+++ src/main/java/org/isf/encounter/rest/EncounterController.java
@@ -214,7 +214,7 @@ public class EncounterController {
 			throw new OHAPIException(new OHExceptionMessage("The encounter found is not for the patient with code "+patientCode));
 		}
 
-		if (encounter.getStatus() == EncounterStatus.CANCELLED) {
+		if (encounterToUpdate.getStatus() == EncounterStatus.CANCELLED) {
 			throw new OHAPIException(new OHExceptionMessage("You cannot modify the canceled encounter."));
 		}
 
